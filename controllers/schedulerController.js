2d375ba6a4dd7f4018e1a005590d187d285a2f2f
Danikhan786 <158603097+Danikhan786@users.noreply.github.com>
2024-08-08 17:36:30 +0500

    remove crone job

--- controllers/schedulerController.js
+++ controllers/schedulerController.js
@@ -26,13 +26,6 @@ const createScheduler = asyncHandler(async (req, res) => {
         });
 
         const createdScheduler = await newScheduler.save();
-
-        // Schedule a cron job based on the created scheduler
-        const cronExpression = getCronExpression(time, day);
-        cron.schedule(cronExpression, async () => {
-            await sendNotification(userId, notificationType);
-        });
-
         res.status(201).json(createdScheduler);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -91,65 +84,65 @@ const deleteScheduler = asyncHandler(async (req, res) => {
     }
 });
 
-// Function to create cron expression based on time and day
-const getCronExpression = (time, day) => {
-    const [hour, minute] = time.split(':');
-    let dayOfWeek;
+// // Function to create cron expression based on time and day
+// const getCronExpression = (time, day) => {
+//     const [hour, minute] = time.split(':');
+//     let dayOfWeek;
 
-    switch (day.toLowerCase()) {
-        case 'sunday':
-            dayOfWeek = '0';
-            break;
-        case 'monday':
-            dayOfWeek = '1';
-            break;
-        case 'tuesday':
-            dayOfWeek = '2';
-            break;
-        case 'wednesday':
-            dayOfWeek = '3';
-            break;
-        case 'thursday':
-            dayOfWeek = '4';
-            break;
-        case 'friday':
-            dayOfWeek = '5';
-            break;
-        case 'saturday':
-            dayOfWeek = '6';
-            break;
-        case 'everyday':
-            dayOfWeek = '*';
-            break;
-        default:
-            dayOfWeek = '*';
-    }
+//     switch (day.toLowerCase()) {
+//         case 'sunday':
+//             dayOfWeek = '0';
+//             break;
+//         case 'monday':
+//             dayOfWeek = '1';
+//             break;
+//         case 'tuesday':
+//             dayOfWeek = '2';
+//             break;
+//         case 'wednesday':
+//             dayOfWeek = '3';
+//             break;
+//         case 'thursday':
+//             dayOfWeek = '4';
+//             break;
+//         case 'friday':
+//             dayOfWeek = '5';
+//             break;
+//         case 'saturday':
+//             dayOfWeek = '6';
+//             break;
+//         case 'everyday':
+//             dayOfWeek = '*';
+//             break;
+//         default:
+//             dayOfWeek = '*';
+//     }
 
-    return `${minute} ${hour} * * ${dayOfWeek}`;
-};
+//     return `${minute} ${hour} * * ${dayOfWeek}`;
+// };
 
-// Check for expired schedules and remove them
-const checkExpiredSchedules = async () => {
-    try {
-        const now = new Date();
-        const expiredSchedules = await Scheduler.find({ time: { $lt: now } });
+// // Check for expired schedules and remove them
+// const checkExpiredSchedules = async () => {
+//     try {
+//         const now = new Date();
+//         const expiredSchedules = await Scheduler.find({ time: { $lt: now } });
 
-        for (const schedule of expiredSchedules) {
-            await Scheduler.findByIdAndDelete(schedule._id);
-        }
-    } catch (error) {
-        console.error("Error checking expired schedules:", error.message);
-    }
-};
+//         for (const schedule of expiredSchedules) {
+//             await Scheduler.findByIdAndDelete(schedule._id);
+//         }
+//     } catch (error) {
+//         console.error("Error checking expired schedules:", error.message);
+//     }
+// };
 
-// Schedule the cron job to run every 30 seconds
-cron.schedule('*/30 * * * * *', async () => {
-    console.log('Running a task every 30 seconds');
-    await checkExpiredSchedules();
-});
+// // Schedule the cron job to run every 30 seconds
+// cron.schedule('*/30 * * * * *', async () => {
+//     console.log('Running a task every 30 seconds');
+//     await checkExpiredSchedules();
+// });
 
 
 // Schedule the cron job to check for expired schedules every day at midnight
 // cron.schedule('0 0 * * *', checkExpiredSchedules);
 
-module.exports = { getAllSchedulers, createScheduler, getSchedulerById, updateScheduler, deleteScheduler, checkExpiredSchedules };
+module.exports = { getAllSchedulers, createScheduler, getSchedulerById, updateScheduler, deleteScheduler, };
